c9751e5180c6c70210a41dcdd9659306c50de0d2
Will Blanton <will.blanton.804@gmail.com>
2015-12-11 16:42:38 -0500

    Init Commit
    Time Saver <3

---
+++ source/PlayState.hx
@@ -0,0 +1,17 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create():Void 
+	{
+		super.create();
+	}
+	
+	override public function update(elapsed:Float):Void 
+	{
+		super.update(elapsed);
+	}
+}
